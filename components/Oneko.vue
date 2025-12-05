17dae4c4f2e52b87d45d2075e2b418ad3dd6fe42
mrbee nopro <127410361+mrbeeenopro@users.noreply.github.com>
2025-12-05 12:25:21 +0700

    Add Oneko component with script loading on mount

---
+++ components/Oneko.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { onMounted } from 'vue';
+
+
+onMounted(() => {
+
+    if (process.client && !document.querySelector('script[src="/assets/oneko.js"]')) {
+        
+  
+        const script = document.createElement('script');
+        
+ 
+        script.src = "/assets/oneko.js"; 
+        
+        script.async = true;
+        
+
+        document.body.appendChild(script);
+        
+
+    }
+});
+</script>
+
+<template>
+    </template>
