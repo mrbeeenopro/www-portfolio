cdfc2dac279a08ceb62e277b2ee982e760f72dc6
mrbee nopro <127410361+mrbeeenopro@users.noreply.github.com>
2025-12-05 11:32:35 +0700

    Load onekoscript asynchronously and initialize

--- plugins/oneko.client.ts
+++ plugins/oneko.client.ts
@@ -3,6 +3,13 @@ export default defineNuxtPlugin(() => {
     const script = document.createElement("script");
     script.src = "https://unpkg.com/oneko@latest/oneko.js";
     script.async = true;
+    script.onload = () => {
+      if (window.oneko) {
+        window.oneko({
+          scale: 1,   
+        });
+      }
+    };
     document.body.appendChild(script);
   }
 });
